e249f66f8201805849046187508b84e981f60271
Shubham Khandelwal <shubham.khandelwal@abjima.com>
2022-07-15 18:35:30 +0530

    15 july code interface

---
+++ 15_July/Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons.Library
+{
+    public  class ConcreteRegularPolygon
+    {
+        public int NumberOfSides { get; set; }
+
+
+
+        public int SideLength;
+        //public int Sidelength
+        //{
+        //    get {  return SideLength; }
+        //    set { SideLength = value; }
+        //}
+
+
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+
+        }
+        public double GetPerimeter()
+        {
+            return NumberOfSides* SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
